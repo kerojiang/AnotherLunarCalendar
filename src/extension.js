8ad71559254f7ddc0f6edc3f92163d40e875cc6a
kerojiang <kero_jiang@126.com>
2020-11-26 22:53:52 +0800

    增加logo,json数据获取

--- src/extension.js
+++ src/extension.js
@@ -1,4 +1,4 @@
-const { St, Clutter } = imports.gi;
+const { St, Clutter, Soup, GLib } = imports.gi;
 const Main = imports.ui.main;
 const Path = imports.misc.extensionUtils.getCurrentExtension();
 const SettingHelper = Path.imports.settingHelper;
@@ -8,6 +8,10 @@ let panelButton;
 //配置信息
 let settings;
 
+//网络session
+let webSession;
+
+//初始化插件
 function init() {
   // Create a Button with "Hello World" text
   panelButton = new St.Bin({
@@ -23,6 +27,7 @@ function init() {
   print(folderPath);
 }
 
+//启动插件
 function enable() {
   //读取配置文件
   settings = SettingHelper.getSettings();
@@ -33,8 +38,64 @@ function enable() {
   log("初始化插件");
 }
 
+//结束插件
 function disable() {
   // Remove the added button from panel
   Main.panel._rightBox.remove_child(panelButton);
   log("取消初始化插件");
+}
+
+
+//读取本地json数据
+function _readLocalData() {
+  //检查本地数据
+  var jsonPath = "";
+
+  try {
+    let [ok, contents] = GLib.file_get_contents(jsonPath);
+
+    if (ok) {
+      let cdata = JSON.parse(contents);
+      log(cdata);
+    }
+  } catch (err) {
+
+  }
+}
+
+
+//写入本地数据
+function _writeLocalData(cdata) {
+  var jsonPath = "";
+  try {
+    let contents = JSON.stringify(cdata, null, "\t");
+    GLib.file_set_contents(jsonPath, contents);
+  } catch (err) {
+
+  }
+
+}
+
+//获取农历数据
+function _getLunarData(year, month) {
+  var url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=" + year + "年" + month + "月&resource_id=39043&format=json&tn=wisetpl";
+
+  if (!webSession) {
+    webSession = Soup.Session.new();
+    webSession.set_property(Soup.SESSION_USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36");
+  }
+  try {
+    let message = Soup.Message.new('GET', url);
+    let status_code = webSession.send_message(message);
+    if (status_code == Soup.Status.OK) {
+
+      log(message.response_body.data);
+      let dataObj = JSON.parse(message.response_body.data);
+
+
+    }
+  } catch (error) {
+
+  }
+
 }
